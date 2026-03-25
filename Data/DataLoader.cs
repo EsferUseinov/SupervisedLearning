005a4819bdb7fedb6b8fdbf6c27687da4e66d85c
Esfer <useinovesfer27@gmail.com>
2026-03-25 17:06:49 +0200

    feat(data): implement data operations

--- Data/DataLoader.cs
+++ Data/DataLoader.cs
@@ -4,6 +4,25 @@ public static class DataLoader
 {
     public static DataSet LoadFromCsv(string path) => throw new NotImplementedException();
 
-    public static DataSet GenerateSynthetic(int samples, int inputSize, int outputSize, int seed) =>
-        throw new NotImplementedException();
+    public static DataSet GenerateSynthetic(int samples, int inputSize, int outputSize, int seed)
+    {
+        var rng = new Random(seed);
+        var result = new DataSample[samples];
+        for (int i = 0; i < samples; i++)
+        {
+            var input = new double[inputSize];
+            double sum = 0.0;
+            for (int j = 0; j < inputSize; j++)
+            {
+                input[j] = rng.NextDouble() * 2.0 - 1.0;
+                sum += input[j];
+            }
+            var label = new double[outputSize];
+            int classIndex = sum > 0.0 ? 0 : 1;
+            if (classIndex < outputSize)
+                label[classIndex] = 1.0;
+            result[i] = new DataSample(input, label);
+        }
+        return new DataSet(result);
+    }
 }
--- Data/DataSet.cs
+++ Data/DataSet.cs
@@ -6,7 +6,37 @@ public class DataSet
 
     public DataSet(DataSample[] samples) => Samples = samples;
 
-    public DataSet[] TrainTestSplit(double trainRatio) => throw new NotImplementedException();
-    public DataSample[][] CreateBatches(int batchSize) => throw new NotImplementedException();
-    public void Shuffle(int seed) => throw new NotImplementedException();
+    public void Shuffle(int seed)
+    {
+        var rng = new Random(seed);
+        for (int i = Samples.Length - 1; i > 0; i--)
+        {
+            int j = rng.Next(i + 1);
+            (Samples[i], Samples[j]) = (Samples[j], Samples[i]);
+        }
+    }
+
+    public DataSample[][] CreateBatches(int batchSize)
+    {
+        int batchCount = (int)Math.Ceiling((double)Samples.Length / batchSize);
+        var batches = new DataSample[batchCount][];
+        for (int i = 0; i < batchCount; i++)
+        {
+            int start = i * batchSize;
+            int length = Math.Min(batchSize, Samples.Length - start);
+            batches[i] = new DataSample[length];
+            Array.Copy(Samples, start, batches[i], 0, length);
+        }
+        return batches;
+    }
+
+    public DataSet[] TrainTestSplit(double trainRatio)
+    {
+        int trainCount = (int)(Samples.Length * trainRatio);
+        var train = new DataSample[trainCount];
+        var test = new DataSample[Samples.Length - trainCount];
+        Array.Copy(Samples, 0, train, 0, trainCount);
+        Array.Copy(Samples, trainCount, test, 0, test.Length);
+        return new[] { new DataSet(train), new DataSet(test) };
+    }
 }
