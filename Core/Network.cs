a8dbdb198a59abf0be690817fbb0b94647f69104
Esfer <useinovesfer27@gmail.com>
2026-03-26 12:09:06 +0200

    feat(core): implement forward, backward, and clone

--- Core/Network.cs
+++ Core/Network.cs
@@ -8,8 +8,35 @@ public class Network
 
     public IReadOnlyList<ILayer> Layers => _layers;
 
-    public void AddLayer(ILayer layer) => throw new NotImplementedException();
-    public double[] Forward(double[] input) => throw new NotImplementedException();
-    public double[] Backward(double[] lossGradient) => throw new NotImplementedException();
-    public Network Clone() => throw new NotImplementedException();
+    public void AddLayer(ILayer layer)
+    {
+        if (_layers.Count > 0 && _layers[^1].OutputSize != layer.InputSize)
+            throw new ArgumentException(
+                $"Layer input size {layer.InputSize} does not match previous layer output size {_layers[^1].OutputSize}.");
+        _layers.Add(layer);
+    }
+
+    public double[] Forward(double[] input)
+    {
+        double[] current = input;
+        foreach (var layer in _layers)
+            current = layer.Forward(current);
+        return current;
+    }
+
+    public double[] Backward(double[] lossGradient)
+    {
+        double[] grad = lossGradient;
+        for (int i = _layers.Count - 1; i >= 0; i--)
+            grad = _layers[i].Backward(grad);
+        return grad;
+    }
+
+    public Network Clone()
+    {
+        var clone = new Network();
+        foreach (var layer in _layers)
+            clone._layers.Add(layer.Clone());
+        return clone;
+    }
 }
